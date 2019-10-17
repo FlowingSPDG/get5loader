d311a9cc4603135818aa9b5aabb7a6a24a614da4
Flowing <bitoreieru@gmail.com>
2019-10-18 07:35:50 +0900

    FUCK.

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"./get5"
-	_ "fmt"
+	"fmt"
 	"github.com/go-ini/ini"
-	_ "github.com/gorilla/mux"
-	_ "log"
-	_ "net/http"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
 )
 
 type Config struct {
@@ -50,53 +50,52 @@ func init() {
 }
 
 func main() {
-	/*
-		r := mux.NewRouter()
-		r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-		//s := r.Host("get5.flowing.tokyo").Subrouter()
-		r.HandleFunc("/", get5.HomeHandler).Methods("GET")
-		r.HandleFunc("/login", get5.LoginHandler).Methods("GET")
-		r.HandleFunc("/logout", get5.LogoutHandler).Methods("GET")
+	r := mux.NewRouter()
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-		r.HandleFunc("/match/create", get5.MatchCreateHandler)             // GET/POST
-		r.HandleFunc("/match/{matchID}", get5.MatchHandler)                // ?
-		r.HandleFunc("/match/{matchID}/config", get5.MatchConfigHandler)   // ?
-		r.HandleFunc("/match/{matchID}/cancel", get5.MatchCancelHandler)   // ?
-		r.HandleFunc("/match/{matchID}/rcon", get5.MatchRconHandler)       // ?
-		r.HandleFunc("/match/{matchID}/pause", get5.MatchPauseHandler)     // ?
-		r.HandleFunc("/match/{matchID}/unpause", get5.MatchUnpauseHandler) // ?
-		r.HandleFunc("/match/{matchID}/adduser", get5.MatchAdduserHandler) // ?
-		//r.HandleFunc("/match/{matchID}/sendconfig", get5.MatchSendConfigHandler) // ?
-		r.HandleFunc("/match/{matchID}/backup", get5.MatchBackupHandler).Methods("GET") // GET
+	//s := r.Host("get5.flowing.tokyo").Subrouter()
+	r.HandleFunc("/", get5.HomeHandler).Methods("GET")
+	r.HandleFunc("/login", get5.LoginHandler).Methods("GET")
+	r.HandleFunc("/logout", get5.LogoutHandler).Methods("GET")
 
-		r.HandleFunc("/match/{matchID}/finish", get5.MatchFinishHandler).Methods("POST")                                             // POST
-		r.HandleFunc("/match/{matchID}/map/{mapNumber}/start", get5.MatchMapStartHandler).Methods("POST")                            // POST
-		r.HandleFunc("/match/{matchID}/map/{mapNumber}/update", get5.MatchMapUpdateHandler).Methods("POST")                          // POST
-		r.HandleFunc("/match/{matchID}/map/{mapNumber}/finish", get5.MatchMapFinishHandler).Methods("POST")                          // POST
-		r.HandleFunc("/match/{matchID}/map/{mapNumber}/player/{steamid64}/update", get5.MatchMapPlayerUpdateHandler).Methods("POST") // POST
+	r.HandleFunc("/match/create", get5.MatchCreateHandler)             // GET/POST
+	r.HandleFunc("/match/{matchID}", get5.MatchHandler)                // ?
+	r.HandleFunc("/match/{matchID}/config", get5.MatchConfigHandler)   // ?
+	r.HandleFunc("/match/{matchID}/cancel", get5.MatchCancelHandler)   // ?
+	r.HandleFunc("/match/{matchID}/rcon", get5.MatchRconHandler)       // ?
+	r.HandleFunc("/match/{matchID}/pause", get5.MatchPauseHandler)     // ?
+	r.HandleFunc("/match/{matchID}/unpause", get5.MatchUnpauseHandler) // ?
+	r.HandleFunc("/match/{matchID}/adduser", get5.MatchAdduserHandler) // ?
+	//r.HandleFunc("/match/{matchID}/sendconfig", get5.MatchSendConfigHandler) // ?
+	r.HandleFunc("/match/{matchID}/backup", get5.MatchBackupHandler).Methods("GET") // GET
 
-		r.HandleFunc("/matches", get5.MatchesHandler)                // ?
-		r.HandleFunc("/matches/{userID}", get5.MatchesWithIDHandler) // ?
-		r.HandleFunc("/mymatches", get5.MyMatchesHandler)            // ?
+	r.HandleFunc("/match/{matchID}/finish", get5.MatchFinishHandler).Methods("POST")                                             // POST
+	r.HandleFunc("/match/{matchID}/map/{mapNumber}/start", get5.MatchMapStartHandler).Methods("POST")                            // POST
+	r.HandleFunc("/match/{matchID}/map/{mapNumber}/update", get5.MatchMapUpdateHandler).Methods("POST")                          // POST
+	r.HandleFunc("/match/{matchID}/map/{mapNumber}/finish", get5.MatchMapFinishHandler).Methods("POST")                          // POST
+	r.HandleFunc("/match/{matchID}/map/{mapNumber}/player/{steamid64}/update", get5.MatchMapPlayerUpdateHandler).Methods("POST") // POST
 
-		r.HandleFunc("/team/create", get5.TeamCreateHandler)              // GET/POST
-		r.HandleFunc("/team/{teamID}", get5.TeamHandler).Methods("GET")   // GET
-		r.HandleFunc("/team/{teamID}/edit", get5.TeamEditHandler)         // GET/POST
-		r.HandleFunc("/team/{teamID}/delete", get5.TeamDeleteHandler)     // ?
-		r.HandleFunc("/teams/{userID}", get5.TeamsHandler).Methods("GET") // GET
-		r.HandleFunc("/myteams", get5.MyTeamsHandler).Methods("GET")      // GET
+	r.HandleFunc("/matches", get5.MatchesHandler)                // ?
+	r.HandleFunc("/matches/{userID}", get5.MatchesWithIDHandler) // ?
+	r.HandleFunc("/mymatches", get5.MyMatchesHandler)            // ?
 
-		r.HandleFunc("/server/create", get5.ServerCreateHandler)                           // GET/POST
-		r.HandleFunc("/server/{serverid}/edit", get5.ServerEditHandler)                    // GET/POST
-		r.HandleFunc("/server/{serverid}/delete", get5.ServerDeleteHandler).Methods("GET") // GET
-		r.HandleFunc("/myservers", get5.MyServersHandler)                                  // ?
+	r.HandleFunc("/team/create", get5.TeamCreateHandler)              // GET/POST
+	r.HandleFunc("/team/{teamID}", get5.TeamHandler).Methods("GET")   // GET
+	r.HandleFunc("/team/{teamID}/edit", get5.TeamEditHandler)         // GET/POST
+	r.HandleFunc("/team/{teamID}/delete", get5.TeamDeleteHandler)     // ?
+	r.HandleFunc("/teams/{userID}", get5.TeamsHandler).Methods("GET") // GET
+	r.HandleFunc("/myteams", get5.MyTeamsHandler).Methods("GET")      // GET
 
-		r.HandleFunc("/user/{userID}", get5.UserHandler)
-		r.Methods("GET", "POST")
-		http.Handle("/", r)
-		fmt.Println("RUNNING")
-		log.Fatal(http.ListenAndServe(HOST, nil))
-	*/
-	get5.MySQLGetGameServerData(SQLHost, SQLUser, SQLPass, SQLDBName, SQLPort)
+	r.HandleFunc("/server/create", get5.ServerCreateHandler)                           // GET/POST
+	r.HandleFunc("/server/{serverid}/edit", get5.ServerEditHandler)                    // GET/POST
+	r.HandleFunc("/server/{serverid}/delete", get5.ServerDeleteHandler).Methods("GET") // GET
+	r.HandleFunc("/myservers", get5.MyServersHandler)                                  // ?
+
+	r.HandleFunc("/user/{userID}", get5.UserHandler)
+	r.Methods("GET", "POST")
+	http.Handle("/", r)
+	fmt.Println("RUNNING")
+	log.Fatal(http.ListenAndServe(HOST, nil))
+
 }
