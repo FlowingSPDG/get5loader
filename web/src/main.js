ab7423a98cdae12d2f5c0b547ba7dc893b970ac1
Flowing <30292185+ruk1ium@users.noreply.github.com>
2019-10-28 14:15:57 +0900

    Update main.js

--- web/src/main.js
+++ web/src/main.js
@@ -3,12 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
 import VueSession from 'vue-session'
+Vue.use(VueSession)
 
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-
-Vue.use(VueSession)
+import locale from 'element-ui/lib/locale/lang/ja'
+Vue.use(ElementUI, {locale})
 
 Vue.config.productionTip = false
 
