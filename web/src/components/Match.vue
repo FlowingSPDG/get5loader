99d12b679f63e3eaeec0f799ca4ba12fe7284bd4
Flowing <bitoreieru@gmail.com>
2019-10-31 00:40:17 +0900

    route fix

--- web/src/components/Match.vue
+++ web/src/components/Match.vue
@@ -22,11 +22,11 @@
     <div class="container" v-loading="loading" v-cloak>
 
         <h1>
-            <img :src="get_logo_or_flag_link(team1,team2).team1" /> <a :href="'/team/'+team1.id"> {{team1.name}}</a>
+            <img :src="get_logo_or_flag_link(team1,team2).team1" /> <a :href="'/team?teamid='+team1.id"> {{team1.name}}</a>
             {{ matchdata.team1_score }}
             {{ score_symbol(matchdata.team1_score, matchdata.team2_score) }}
             {{ matchdata.team2_score }}
-            <img :src="get_logo_or_flag_link(team1,team2).team2" /> <a :href="'/team/'+team2.id"> {{team2.name}}</a>
+            <img :src="get_logo_or_flag_link(team1,team2).team2" /> <a :href="'/team?teamid='+team2.id"> {{team2.name}}</a>
 
             <div class="dropdown dropdown-header pull-right" v-if="user.adminaccess == true && matchdata.live && match.pending">
                 <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
