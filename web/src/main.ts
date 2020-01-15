816c025b67aa6cdceb7533f900ab253f80fdd8fb
Flowing <bitoreieru@gmail.com>
2020-01-16 04:06:38 +0900

    created /web/src/utils/api.vue for mixin functions

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,10 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
+import api from './utils/api.vue'
+
+// ElementUI...
 import 'element-ui/lib/theme-chalk/index.css'
 const locale = require('element-ui/lib/locale/lang/ja')
 Vue.use(ElementUI, { locale })
 Vue.config.productionTip = false
+
+// Mixin ./utils/api.vue
+Vue.mixin(api)
 
 new Vue({
   router,
