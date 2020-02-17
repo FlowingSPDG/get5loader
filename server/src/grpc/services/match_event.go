0cc6bc358162b20619441af2753ac4e67def9c33
Flowing <bitoreieru@gmail.com>
2020-02-17 09:12:37 +0900

    Added receiver for eventmap

--- server/src/grpc/services/match_event.go
+++ server/src/grpc/services/match_event.go
@@ -14,8 +14,9 @@ import (
 )
 
 type Events struct {
-	Finished chan (bool) // Stream is closed or not
-	Event    chan (*pb.MatchEventReply)
+	Finished  chan (bool) // Stream is closed or not
+	Event     chan (*pb.MatchEventReply)
+	Receivers uint16 // number of receivers
 }
 
 type EventsMap struct {
@@ -31,14 +32,17 @@ func (e *EventsMap) Write(key int32, Event *pb.MatchEventReply, finished bool) e
 	//defer e.mux.Unlock()
 	if e.Event[key] == nil {
 		e.Event[key] = &Events{
-			Finished: make(chan bool, 1),
-			Event:    make(chan *pb.MatchEventReply, 1),
+			Finished:  make(chan bool, 1),
+			Event:     make(chan *pb.MatchEventReply, 1),
+			Receivers: 0,
 		}
 	}
-	log.Printf("[gRPC] Writing for key %d\n", key)
-	e.Event[key].Finished <- finished
-	e.Event[key].Event <- Event
-	log.Printf("[gRPC] Writing for key %d DONE\n", key)
+	for i := uint16(0); i < e.Event[key].Receivers; i++ {
+		log.Printf("[gRPC] Writing for key %d\n", key)
+		e.Event[key].Finished <- finished
+		e.Event[key].Event <- Event
+		log.Printf("[gRPC] Writing for key %d DONE\n", key)
+	}
 	return nil
 }
 
@@ -46,15 +50,27 @@ func (e *EventsMap) Read(key int32) (*pb.MatchEventReply, bool, error) {
 	log.Printf("[gRPC] Reading for key %d\n", key)
 	e.mux.Lock()
 	defer e.mux.Unlock()
+	log.Printf("[gRPC] Received Event on Read(). Event : %v Finished : %v\n", ev, fi)
 	if val, ok := e.Event[key]; ok {
-		ev := <-val.Event
-		fi := <-val.Finished
-		log.Printf("[gRPC] Received Event on Read(). Event : %v Finished : %v\n", ev, fi)
-		log.Printf("[gRPC] Reading for key %d DONE\n", key)
-		return ev, fi, nil
+		for i := uint16(1); i < val.Receivers; i++ {
+			ev := <-val.Event
+			fi := <-val.Finished
+			log.Printf("[gRPC] Reading for key %d DONE\n", key)
+		}
+		return <-val.Event, <-val.Finished, nil
 	}
 	return nil, false, fmt.Errorf("Not Found")
+}
 
+func (e *EventsMap) AddReceiver(key int32) error {
+	log.Printf("[gRPC] Adding for key %d\n", key)
+	e.mux.Lock()
+	defer e.mux.Unlock()
+	if val, ok := e.Event[key]; ok {
+		val.Receivers++
+		return nil
+	}
+	return fmt.Errorf("Not Found")
 }
 
 func (e *EventsMap) CloseChannels(key int32) error {
@@ -92,6 +108,7 @@ func (s Server) MatchEvent(req *pb.MatchEventRequest, srv pb.Get5_MatchEventServ
 
 	lastevent := initev
 	for { //go func(){}() ?
+		MatchesStream.AddReceiver(matchid)
 		senddata, finished, err := MatchesStream.Read(matchid)
 		if err != nil {
 			log.Printf("[gRPC] Unknown ERROR : %v\n", err)
