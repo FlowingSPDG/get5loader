20e10b3a9823a3a8af2924680484ecc1bd8a7e44
Flowing <bitoreieru@gmail.com>
2020-02-19 00:52:56 +0900

    Update 20200219005005-create_tables.sql

--- migration/migrate/schema/20200219005005-create_tables.sql
+++ migration/migrate/schema/20200219005005-create_tables.sql
@@ -1,5 +1,6 @@
 
 -- +migrate Up
-ALTER TABLE `round_stats` RENAME COLUMN `fisrt_victim_steamid` TO `first_victim_steamid`
+ALTER TABLE `round_stats` RENAME COLUMN `fisrt_victim_steamid` TO `first_victim_steamid`;
+
 -- +migrate Down
-ALTER TABLE `round_stats` RENAME COLUMN `first_victim_steamid` TO `fisrt_victim_steamid`
+ALTER TABLE `round_stats` RENAME COLUMN `first_victim_steamid` TO `fisrt_victim_steamid`;
