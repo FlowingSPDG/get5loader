6c76cb51bdf0362597b4cc59dba0189950a8083f
Flowing <bitoreieru@gmail.com>
2020-02-19 01:03:11 +0900

    Update 20200219005005-create_tables.sql

--- migration/migrate/schema/20200219005005-create_tables.sql
+++ migration/migrate/schema/20200219005005-create_tables.sql
@@ -1,6 +1,7 @@
 
 -- +migrate Up
 ALTER TABLE `round_stats` CHANGE `fisrt_victim_steamid` `first_victim_steamid` varchar(40);
+ALTER TABLE `round_stats` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 -- +migrate Down
 ALTER TABLE `round_stats` CHANGE `first_victim_steamid` `fisrt_victim_steamid` varchar(40);
