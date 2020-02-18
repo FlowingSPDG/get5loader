b083ae7ce32fed1d0c404a2edc06d3f21bb93674
Flowing <bitoreieru@gmail.com>
2020-02-19 01:22:00 +0900

    Update 20200219005005-create_tables.sql

--- migration/migrate/schema/20200219005005-create_tables.sql
+++ migration/migrate/schema/20200219005005-create_tables.sql
@@ -10,3 +10,5 @@ ALTER TABLE `round_stats` ADD `map_number` int(11) DEFAULT NULL ;
 
 -- +migrate Down
 ALTER TABLE `round_stats` CHANGE `first_victim_steamid` `fisrt_victim_steamid` varchar(40);
+ALTER TABLE `round_stats` DROP `map_number`;
+ALTER TABLE `round_stats` DROP `winner`;
