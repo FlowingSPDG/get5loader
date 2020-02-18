55a72817e7c8aa388e32903b8510c5a9dbe6078f
Flowing <bitoreieru@gmail.com>
2020-02-19 01:05:37 +0900

    Update 20200219005005-create_tables.sql

--- migration/migrate/schema/20200219005005-create_tables.sql
+++ migration/migrate/schema/20200219005005-create_tables.sql
@@ -2,6 +2,7 @@
 -- +migrate Up
 ALTER TABLE `round_stats` CHANGE `fisrt_victim_steamid` `first_victim_steamid` varchar(40);
 ALTER TABLE `round_stats` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `round_stats` DROP FOREIGN KEY `round_stats_ibfk_2`;
 
 -- +migrate Down
 ALTER TABLE `round_stats` CHANGE `first_victim_steamid` `fisrt_victim_steamid` varchar(40);
