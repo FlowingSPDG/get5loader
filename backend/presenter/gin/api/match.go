520d8275004b55646518795569f218762b707cac
Shugo Kawamura <shugo_k@flowing.tokyo>
2023-08-08 02:01:59 +0900

    presenterもentityを使用するように

--- backend/presenter/gin/api/match.go
+++ backend/presenter/gin/api/match.go
@@ -21,11 +21,11 @@ func NewMatchPresenter() MatchPresenter {
 // Handle implements MatchPresenter.
 func (mp *matchPresenter) Handle(c *gin.Context, m *entity.Match) {
 	data := match{
-		ID:         string(m.ID),
-		UserID:     string(m.UserID),
-		Team1ID:    string(m.Team1ID),
-		Team2ID:    string(m.Team2ID),
-		Winner:     nil,
+		ID:         m.ID,
+		UserID:     m.UserID,
+		Team1ID:    m.Team1ID,
+		Team2ID:    m.Team2ID,
+		Winner:     m.Winner,
 		Cancelled:  m.Status == entity.MATCH_STATUS_CANCELLED,
 		StartTime:  m.StartTime,
 		EndTime:    m.EndTime,
@@ -35,14 +35,8 @@ func (mp *matchPresenter) Handle(c *gin.Context, m *entity.Match) {
 		Team1Score: int(m.Team1Score),
 		Team2Score: int(m.Team2Score),
 		Forfeit:    *m.Forfeit,
-		ServerID:   string(m.ServerID),
+		ServerID:   m.ServerID,
 		Status:     m.Status.String(),
 	}
-
-	if m.Winner != nil {
-		winner := string(*m.Winner)
-		data.Winner = &winner
-	}
-
 	c.JSON(200, data)
 }
--- backend/presenter/gin/api/models.go
+++ backend/presenter/gin/api/models.go
@@ -1,60 +1,61 @@
 package api
 
 // 受け取ったデータを特定の形式に変換して返すpresenter
-// このpackageでは極力根底型を使うようにする
 
 import (
 	"time"
+
+	"github.com/FlowingSPDG/get5-web-go/backend/entity"
 )
 
 type player struct {
-	ID      string `json:"id"`
-	TeamID  string `json:"team_id"`
-	SteamID uint64 `json:"steam_id"`
-	Name    string `json:"name"`
+	ID      entity.PlayerID `json:"id"`
+	TeamID  entity.TeamID   `json:"team_id"`
+	SteamID entity.SteamID  `json:"steam_id"`
+	Name    string          `json:"name"`
 }
 
 type team struct {
-	ID         string   `json:"id"`
-	UserID     string   `son:"user_id"`
-	Name       string   `son:"name"`
-	Tag        string   `son:"tag"`
-	Flag       string   `son:"flag"`
-	Logo       string   `son:"logo"`
-	Players    []player `json:"players"`
-	PublicTeam bool     `son:"public_team"`
+	ID         entity.TeamID `json:"id"`
+	UserID     entity.UserID `son:"user_id"`
+	Name       string        `son:"name"`
+	Tag        string        `son:"tag"`
+	Flag       string        `son:"flag"`
+	Logo       string        `son:"logo"`
+	Players    []player      `json:"players"`
+	PublicTeam bool          `son:"public_team"`
 }
 
 type mapStats struct {
-	ID         string    `json:"id"`
-	MatchID    string    `json:"match_id"`
-	MapNumber  int       `json:"map_number"`
-	MapName    string    `json:"map_name"`
-	StartTime  time.Time `json:"-"`
-	EndTime    time.Time `json:"-"`
-	Winner     string    `json:"winner"`
-	Team1Score int       `json:"team1_score"`
-	Team2Score int       `json:"team2_score"`
+	ID         entity.MapStatsID `json:"id"`
+	MatchID    entity.MatchID    `json:"match_id"`
+	MapNumber  int               `json:"map_number"`
+	MapName    string            `json:"map_name"`
+	StartTime  time.Time         `json:"-"`
+	EndTime    time.Time         `json:"-"`
+	Winner     string            `json:"winner"`
+	Team1Score int               `json:"team1_score"`
+	Team2Score int               `json:"team2_score"`
 }
 
 type match struct {
-	ID         string     `json:"id"`
-	UserID     string     `json:"user_id"`
-	Team1ID    string     `json:"team1"`
-	Team2ID    string     `json:"team2"`
-	Winner     *string    `json:"winner"`
-	Cancelled  bool       `json:"cancelled"`
-	StartTime  *time.Time `json:"start_time"`
-	EndTime    *time.Time `json:"end_time"`
-	MaxMaps    int        `json:"max_maps"`
-	Title      string     `json:"title"`
-	SkipVeto   bool       `json:"skip_veto"`
-	Team1Score int        `json:"team1_score"`
-	Team2Score int        `json:"team2_score"`
-	Forfeit    bool       `json:"forfeit"`
-	ServerID   string     `json:"server_id"`
+	ID         entity.MatchID      `json:"id"`
+	UserID     entity.UserID       `json:"user_id"`
+	Team1ID    entity.TeamID       `json:"team1"`
+	Team2ID    entity.TeamID       `json:"team2"`
+	Winner     *entity.TeamID      `json:"winner"`
+	ServerID   entity.GameServerID `json:"server_id"`
+	Cancelled  bool                `json:"cancelled"`
+	StartTime  *time.Time          `json:"start_time"`
+	EndTime    *time.Time          `json:"end_time"`
+	MaxMaps    int                 `json:"max_maps"`
+	Title      string              `json:"title"`
+	SkipVeto   bool                `json:"skip_veto"`
+	Team1Score int                 `json:"team1_score"`
+	Team2Score int                 `json:"team2_score"`
+	Forfeit    bool                `json:"forfeit"`
 
-	MapStatsIDs []string `json:"map_stats"`
+	MapStatsIDs []entity.MapStatsID `json:"map_stats"`
 
 	Status string `json:"status"`
 }
