64e8a88ed52d78b40f3ee8daf286da1251611aa5
Shugo Kawamura <shugo_k@flowing.tokyo>
2023-08-08 04:26:31 +0900

    ValidateJWT usecaseのUT追加

---
+++ backend/usecase/validate_jwt_test.go
@@ -0,0 +1,59 @@
+package usecase_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/FlowingSPDG/get5-web-go/backend/entity"
+	"github.com/FlowingSPDG/get5-web-go/backend/g5ctx"
+	mock_jwt "github.com/FlowingSPDG/get5-web-go/backend/service/jwt/mock"
+	"github.com/FlowingSPDG/get5-web-go/backend/usecase"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
+)
+
+func TestValidateJWT(t *testing.T) {
+	tt := []struct {
+		name     string
+		input    string
+		expected *entity.TokenUser
+		err      error
+	}{
+		{
+			name:  "success",
+			input: "test",
+			expected: &entity.TokenUser{
+				UserID:  "1",
+				SteamID: 76561198072054549,
+				Admin:   true,
+			},
+			err: nil,
+		},
+		{
+			name:     "invalid token",
+			input:    "invalid",
+			expected: nil,
+			err:      nil,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			ctx := context.Background()
+			ctx = g5ctx.SetOperation(ctx, g5ctx.OperationTypeUser)
+
+			// mock connectorの作成
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			// mock JWTServiceの作成
+			mockJwtService := mock_jwt.NewMockJWTService(ctrl)
+			mockJwtService.EXPECT().ValidateJWT(tc.input).Return(tc.expected, tc.err)
+
+			uc := usecase.NewValidateJWT(mockJwtService)
+			actual, err := uc.Validate(tc.input)
+			assert.Equal(t, tc.expected, actual)
+			assert.Equal(t, tc.err, err)
+		})
+	}
+}
