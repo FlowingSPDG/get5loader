31822a63d211eae70c87fbae06ef6058ef39d790
Shugo Kawamura <shugo_k@flowing.tokyo>
2023-08-14 16:40:05 +0900

    mock生成の修正

--- backend/usecase/usecase_mock.go
+++ backend/usecase/usecase_mock.go
@@ -1,4 +1,12 @@
 package usecase
 
 //go:generate mockgen -source=match.go -destination=mock/match.go
-//go:generate mockgen -source=user.go -destination=mock/login.go
+//go:generate mockgen -source=gameserver.go -destination=mock/gameserver.go
+//go:generate mockgen -source=get5.go -destination=mock/get5.go
+//go:generate mockgen -source=mapstats.go -destination=mock/mapstats.go
+//go:generate mockgen -source=match.go -destination=mock/match.go
+//go:generate mockgen -source=player.go -destination=mock/player.go
+//go:generate mockgen -source=playerstat.go -destination=mock/playerstat.go
+//go:generate mockgen -source=team.go -destination=mock/team.go
+//go:generate mockgen -source=user.go -destination=mock/user.go
+//go:generate mockgen -source=validate_jwt.go -destination=mock/validate_jwt.go
